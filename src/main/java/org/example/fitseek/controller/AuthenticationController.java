30edb9241d2579fa431afde49e00608a10b0f335
fallestlaww <qtensell1@gmail.com>
2025-03-30 00:32:56 +0200

    fix: unnecessary rows in FitseekApplication.java, change controllers annotations in AuthenticationController.java, UserController.java

--- src/main/java/org/example/fitseek/controller/AuthenticationController.java
+++ src/main/java/org/example/fitseek/controller/AuthenticationController.java
@@ -23,10 +23,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
