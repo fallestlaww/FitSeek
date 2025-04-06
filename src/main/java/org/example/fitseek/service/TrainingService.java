9640e6464acb01c8ed12cb4ecdceaeff6336a7da
Pavlo Svitenko <112775112+fallestlaww@users.noreply.github.com>
2025-04-06 18:38:56 +0300

    removing TrainingService.java

--- src/main/java/org/example/fitseek/service/TrainingService.java
+++
@@ -1,8 +0,0 @@
-package org.example.fitseek.service;
-
-import org.example.fitseek.dto.request.TrainingTypeRequest;
-import org.example.fitseek.model.TrainingType;
-
-public interface TrainingService {
-    TrainingType chooseTraining(TrainingTypeRequest request);
-}
